d8758106ded3da4e6f0d8b5bc45007ebf63b0a35
acavi <eacavi10@gmail.com>
2023-07-01 20:08:50 -0500

    config enviroment

---
+++ devtest/src/main/java/com/test/devtest/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.test.devtest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class ProductController {
+
+
+
+    @GetMapping("/product/{productId}/similar")
+    public String getSimilarProducts(@PathVariable(value="productId") String productId) {
+        log.info("Retrieve similar products to product with id: " + productId);
+       // List<ProductDetail> similarProducts = retrieveSortedSimilarProductsUseCase.execute(productId);
+        return "Hello World!";
+
+    }
+
+}
